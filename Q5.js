414f34c2679d1b8fa60807c85e3767fbc052a6a6
Muniza <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-03-05 14:26:47 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,4 @@
+// 5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Hazrat Ali (R.Z)";
+var message = " The greatest loss is wasted time ";
+console.log("".concat(famous_person, " Once Said , \"").concat(message, "\""));
